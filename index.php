b0f006b8946474f9592677c5dcd83ba33c168c2a
Sasha Polunin <polunin-sasha2003@mail.ru>
2022-05-11 15:28:38 +0300

    helloWorld

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Hello world!"; ?>
+    <?php
+        $a = 27;
+        $b = 12;
+        $c = $a**2 + $b**2;
+        echo pow(-1,20 );
+    ?>
+</body>
+</html>
